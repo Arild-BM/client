14514d4ea4923ebee2e7f81117ad06d96def799b
Arild-BM <112947008+Arild-BM@users.noreply.github.com>
2023-07-25 10:08:05 +0200

    1. Upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,201 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+import View from './View'
 
 function App() {
+
+  const [screen, setScreen] = useState('auth');
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [message, setMessage] = useState("");
+  const [delAcc, setDelAcc] = useState("");
+
+  // Create new users
+  const register = async () => {
+    try {
+      const res = await fetch('http://localhost:5000/register', {
+        method: 'POST',
+       body: JSON.stringify({ name: name, username: username, password: password }),
+      headers: { 'Content-Type': 'application/json' }
+      })
+      // const data = await res.json()
+      const data = await res.text()
+      console.log("Melding " + res.status)
+      console.log(data)
+      setMessage(data)
+      setName("")
+      setUsername("")
+      setPassword("")
+      }
+      catch (err) {
+         setMessage(err.message)
+       }
+     }
+
+  // Log in user
+  async function auth() {
+    
+    try {
+      const res = await axios.get('/authenticate', { auth: { username, password } });
+      
+      if (res.data.screen !== undefined) {
+        console.log("Screen: " + res.data.screen)
+        setScreen(res.data.screen);
+      }
+    } catch (e) {
+      setMessage(e.response.data)
+    }
+  };
+
+  const readCookie = async () => {
+    try {
+      const res = await axios.get('/read-cookie');
+      
+      if (res.data.screen !== undefined) {
+        setScreen(res.data.screen);
+      }
+    } catch (e) {
+      setScreen('auth');
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    readCookie();
+  }, []);
+  
+  // Remove messages after 2 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => setMessage(""), 2000);
+    return () => clearTimeout(timer);
+  }, [message]);
+
+  // Let user change password
+  async function saveNewPassword(pw) {
+    try {
+      const res = await fetch('http://localhost:5000/changePw', {
+        method: 'PATCH',
+       body: JSON.stringify({ username: username, password: pw }),
+      headers: { 'Content-Type': 'application/json' }
+      })
+      
+      const data = await res.text()
+      console.log("Melding " + res.status)
+      console.log(data)
+      setMessage(data)
+      setPassword1("")
+      setPassword2("")
+      setPassword("")
+      setUsername("")
+      setScreen("auth")
+    }
+    catch (err) {
+      setMessage(err.message)
+    }
+  }
+
+  // Delete user account
+  async function deleteUser(user) {
+    try {
+      const res = await fetch('http://localhost:5000/deleteUser', {
+        method: 'DELETE',
+       body: JSON.stringify({ username: user }),
+      headers: { 'Content-Type': 'application/json' }
+      })
+      
+      const data = await res.text()
+      console.log("Melding " + res.status)
+      console.log(data)
+      setMessage(data)
+      setDelAcc("")
+    }
+    catch (err) {
+      setMessage(err.message)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Log in screen */}
+      {screen === 'auth'
+        ? <div>
+            <label>Username: </label>
+            <br/>
+            <input type="text" value={username} placeholder = "user ID" autoComplete="off" onChange={e => setUsername(e.target.value)} />
+            <br/>
+            <label>Password: </label>
+            <br/>
+            <input type="password" value={password} placeholder = "password" autoComplete="new-password" onChange={e => setPassword(e.target.value)} />
+            <br/>
+            <button onClick={auth}>Login</button>
+            {message==="" ? <><br/><br/></> : <p className="message">{message}</p>}
+            <button onClick={ () => setScreen('register')}>Register new user</button>
+          </div>
+          // Screen for register new user
+          : screen === 'register'
+          ? <div>
+              <label className="labelmargin">Name: </label>
+              <br/>
+              <input type="text" value = {name} autoComplete="off" placeholder = "Fornavn Etternavn" onChange={e => setName(e.target.value)} />
+              <br/>
+              <label>Username: </label>
+              <br/>
+              <input type="text" value = {username} autoComplete="off" placeholder = "brukernavn" onChange={e => setUsername(e.target.value)} />
+              <br/>
+              <label>Password: </label>
+              <br/>
+              <input type="password" value = {password} autoComplete="new-password" onChange={e => setPassword(e.target.value) } />
+              <br/>
+              <button onClick={
+                name && username && password ? register : () => setMessage("Please fill out all fields!")
+                }>Create user account</button>
+              {message==="" ? <br/> : <p className="message message2">{message}</p>}
+              <button onClick={ () => setScreen('auth')}>Go to login page</button>
+            </div>
+            // Screen for change password
+            : screen === 'changePw'
+            ? <div>
+                <p>Write new password for user "{username}" </p>
+                <br/>
+                <label>New password: </label>
+                <br/>
+                <input type="password" value={password1} onChange={e => setPassword1(e.target.value) } />
+                <br/>
+                <label>Retype password: </label>
+                <br/>
+                <input type="password" value={password2} onChange={e => setPassword2(e.target.value) } />
+                <br/>
+                <button onClick={
+                  password1 === password2 ? () => saveNewPassword(password1) : () => setMessage("Passwords not equal!")
+                  }>Change password</button>
+                {message==="" ? <><br/><br/></> : <p className="message">{message}</p>}
+                <button onClick={ () => setScreen('auth')}>Go to login page</button>
+              </div>
+              // Screen for delete user
+              : screen === 'deleteUser'
+              ? <div>
+                  <p>Delete user account </p>
+                  <br/>
+                  <label>Account to be deleted: </label>
+                  <br/>
+                  <input type="text" value = {delAcc} onChange={e => setDelAcc(e.target.value) } />
+                  <br/>
+                  <button
+                    className="message" 
+                    onClick={
+                      delAcc !== "" ? () => deleteUser(delAcc) : () => setMessage("Input field blank!")
+                    }>Delete account!</button>
+                  {message==="" ? <><br/><br/></> : <p className="message">{message}</p>}
+                  <button onClick={ () => setScreen('auth')}>Go to login page</button>
+                </div>
+                // Screen for Todo list
+        : <View setScreen={setScreen} username={username} setUsername={setUsername} setPassword={setPassword} />
+      }
     </div>
   );
 }
---
+++ src/View.jsx
@@ -0,0 +1,215 @@
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+import { v4 as uuid } from 'uuid';
+
+import sun from "./pictures/icon-sun.svg"
+import moon from "./pictures/icon-moon.svg"
+import check from "./pictures/icon-check.svg"
+import cross from "./pictures/icon-cross.svg"
+
+function View (props) {
+  const { setScreen, username, setUsername, setPassword } = props;
+  
+  const [darkmode, setDarkmode] = useState(true);
+  const [darkmodeIcon, setDarkmodeIcon] = useState(sun);
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState({});
+  const [show, setShow] = useState("All")
+      
+  // Logout
+  const deleteCookie = async () => {
+    try {
+      await axios.get('/clear-cookie');
+      setScreen('auth');
+      setPassword("")
+      setUsername("")
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  
+  // Change color theme
+  function setColor() {
+    darkmode ? document.documentElement.className = "dark" : document.documentElement.className = "light"
+    darkmode ? setDarkmodeIcon(sun) : setDarkmodeIcon(moon)
+    setDarkmode(!darkmode)
+  }
+
+  // Insert new todo
+  async function handleSubmit(event) {
+    event.preventDefault()
+    try {
+      const res = await axios.put('/put-data', {todolist: [...todos, newTodo]});
+      console.log(res.data)
+    } catch (e) {
+      console.log(e);
+    }
+    setTodos(prev => [...prev, newTodo])
+    setNewTodo({todo: ""})
+  }
+
+  
+  useEffect(() => {
+    getData()
+  }, []);
+
+  // Get data from database when application opens
+  async function getData() {
+    try {
+      const res = await axios.get('/get-data');
+      setTodos(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
+  // Toggle Todo as active/done
+  async function updateTodo(id) {
+    const index = todos.findIndex((todo) => todo.id === id )
+    try {
+      const res = await axios.put('/put-data', {
+        todolist: [...todos.slice(0, index),
+                  {todo: todos[index].todo,
+                  active: !todos[index].active,
+                  completed: todos[index].completed,
+                  id: todos[index].id},
+                  ...todos.slice(index + 1)
+        ]});
+      console.log(res.data)
+    } catch (e) {
+      console.log(e);
+    }
+    setTodos([...todos.slice(0, index),
+      {todo: todos[index].todo,
+      active: !todos[index].active,
+      completed: todos[index].completed,
+      id: todos[index].id},
+      ...todos.slice(index + 1)
+    ])
+  }
+
+  // Toggle Todo as ready or not to be deleted (change flag)
+  async function markTodoAsDone(id) {
+    const index = todos.findIndex((todo) => todo.id === id )
+    
+    try {
+      const res = await axios.put('/put-data', {
+        todolist: [...todos.slice(0, index),
+                  {todo: todos[index].todo,
+                  active: todos[index].active,
+                  completed: !todos[index].completed,
+                  id: todos[index].id},
+                  ...todos.slice(index + 1)
+        ]});
+      console.log(res.data)
+    } catch (e) {
+      console.log(e);
+    }
+    setTodos([...todos.slice(0, index),
+      {todo: todos[index].todo,
+      active: todos[index].active,
+      completed: !todos[index].completed,
+      id: todos[index].id},
+      ...todos.slice(index + 1)
+    ]);
+    console.log(index);
+  }
+
+  // Insert new Todo
+  function InsertTodoLine({item, index}) {
+    return (
+      <div key = {index} className = "todo todo2">
+        <img className= {item.active ? 'mark marked mark-pointer' : 'mark mark-pointer' }
+          src = { check }
+          alt = ""
+          onClick = { () => updateTodo(item.id) }
+        />
+        <p className={item.completed ? "input-field deleted" : "input-field" } >
+          {item.todo}
+        </p>
+        <img className = "cross mark-pointer"
+          src = { cross }
+          alt = "Delete-icon"
+          onClick = { () => markTodoAsDone(item.id) }
+        />
+      </div>
+    )
+  }
+
+  // Delete marked Todos
+  async function deleteMarkedTodos() {
+    try {
+      const res = await axios.put('/put-data', {
+        todolist: todos.filter(item => !item.completed)});
+        console.log(res.data)
+    } catch (e) {
+      console.log(e);
+    }
+    setTodos(() => todos.filter(item => !item.completed))
+  }
+
+  return (
+    <div>
+      {/* Header line */}
+      <div className = "header">
+        <h3>TODO</h3>
+        <button onClick={deleteCookie}>Logout</button>
+        {console.log("Username: " + username)}
+        {username === "admin" ?
+          <button onClick={() => setScreen('deleteUser')}>Delete user</button>
+          : <button onClick={() => setScreen('changePw')}>Change PW</button>
+        }
+        {/* Icon for teggle color mode */}
+        <img className="darkmode-icon"
+          onClick = {() => setColor()}
+          src = {darkmodeIcon}
+          alt = "icon for toggle light and dark theme"
+        />
+      </div>
+      {/* Create new Todo item */}
+      <div className = "todo create-line-margin">
+        <p className='mark'></p>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="input-field"
+            type="text" 
+            value={newTodo.todo}
+            onChange={(e) => setNewTodo({
+              todo: e.target.value,
+              active: false,
+              completed: false,
+              id: uuid()
+            })}
+            placeholder="Create a new todo..."
+          />
+        </form>
+      </div>
+      
+      {/* Show Todo list */}
+      <div className="todo-list">
+        {/* Show all Todos */}
+        {show==="All" ? todos.map((item, index) => <InsertTodoLine item={item} index={index}/>): null}
+        {/* Show only active Todos */}
+        {show==="Active" ? todos.filter(todo => todo.active === true).map((item, index) => <InsertTodoLine item={item} index={index}/>): null}
+        {/* Show only completed Todos - ready for deletion */}
+        {show==="Completed" ? todos.filter(todo => todo.completed === true).map((item, index) => <InsertTodoLine item={item} index={index}/>): null}
+        
+        {/* Line with buttons Insert Todo and Delete Completed */}
+        <div className="todo todo3">
+          <p>{todos.length === 0 ? "Insert Todo" : todos.length === 1 ? "1 item left" : todos.length + " items left"}</p>
+          <p className="pointer" onClick = {() => deleteMarkedTodos()}>Delete Completed</p>
+        </div>
+
+        {/* Footer - Choose which Todos to show */}
+        <div className="todo footer">
+          <p className = {show === "All" ? "pointer blue-text" : "pointer"} onClick = {() => setShow("All")}>All</p>
+          <p className = {show === "Active" ? "pointer blue-text" : "pointer"} onClick = {() => setShow("Active")}>Active</p>
+          <p className = {show === "Completed" ? "pointer blue-text" : "pointer"} onClick = {() => setShow("Completed")}>Completed</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default View
